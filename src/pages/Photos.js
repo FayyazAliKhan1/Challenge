3f0a4b7e1b8300f0e78b61917a49617230abc09c
FayyazAliKhan1 <nakhan674@gmail.com>
2021-01-09 19:22:25 +0500

    show all photos instead of 5

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -4,11 +4,11 @@ import PhotosItem from '../components/PhotosItem';
 function Photos() {
   const photosList = useContext(MyContext).photos;
 
-  let some_images = photosList.slice(0, 5);
+  // let some_images = photosList.slice(0, 5);
   // console.log('Some', some_images);
   return (
     <ul>
-      {some_images.map((photo) => {
+      {photosList.map((photo) => {
         return <PhotosItem key={photo.id} photo={photo} />;
       })}
     </ul>
